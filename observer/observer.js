10bd18ec0a33681181071687dba7149d163a6d89
Глотов Дмитрий <glotovdu@it2g.ru>
2019-06-16 21:10:48 +0300

    first commit

---
+++ observer/observer.js
@@ -0,0 +1,47 @@
+/**
+ * Сделать возможность подписываться на изменение значения калькулятора
+ * Для этого реализовать метод subscribe используя патерн наблюдатель
+ *
+ * Для запуска из папки с файлом в консоли прописать
+ * node observer.js
+ */
+class Calc {
+  constructor() {
+    this.result = 0;
+    this.subscriptions = [];
+  }
+
+  eventTrigger() {
+    for (let subscription of this.subscriptions) {
+      subscription(this.result);
+    }
+  }
+
+  increment() {
+    this.result += 1;
+    this.eventTrigger();
+  }
+
+  decrement() {
+    this.result -= 1;
+    this.eventTrigger();
+  }
+
+  subscribe(fn) {
+    // TODO
+    this.subscriptions.push(fn);
+  }
+}
+
+const calc = new Calc();
+
+calc.subscribe(res => console.log(res));
+calc.subscribe(res => console.log(res));
+calc.increment();
+calc.increment();
+calc.increment();
+calc.decrement();
+calc.increment();
+calc.decrement();
+calc.decrement();
+calc.decrement();
