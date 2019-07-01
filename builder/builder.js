da77c5a706fea057d970bf0fd5ce3c04ef224dd0
Глотов Дмитрий <glotovdi@gmail.com>
2019-07-01 11:29:23 +0300

    builder best pratice

--- builder/builder.js
+++ builder/builder.js
@@ -1,31 +1,42 @@
 /** Используется для создания объектов со сложными состояниями. Создание объекта "по-кирпичикам"  */
 
-class pcBuilder {
-  constructor(params) {
-    this.name = params;
+class Pc {
+  constructor(build) {
+
+    this.ram = build.ram;
+    this.cpu = build.cpu;
+    this.ssd = build.ssd;
+    this.hdd = build.hdd;
   }
-  setRam(size) {
-    this.ram = `${size}GB`;
-    return this;
-  }
-  setCPU(name) {
-    this.cpu = name;
-    return this;
-  }
-  setSSD(size) {
-    this.ssd = `${size}GB`;
-    return this;
-  }
-  setHDD(size) {
-    this.hdd = `${size}GB`;
-    return this;
+  static get Builder() {
+    class Builder {
+      constructor() {}
+      setRam(size) {
+        this.ram = `${size}GB`;
+        return this;
+      }
+      setCPU(name) {
+        this.cpu = name;
+        return this;
+      }
+      setSSD(size) {
+        this.ssd = `${size}GB`;
+        return this;
+      }
+      setHDD(size) {
+        this.hdd = `${size}GB`;
+        return this;
+      }
+      build() {
+        return new Pc(this);
+      }
+    }
+    return Builder
   }
 }
 
-var myPc = new pcBuilder('smthngPc');
-console.log(myPc);
-myPc
-  .setCPU(2)
-  .setSSD(2)
-  .setHDD(2);
-console.log(myPc);
+var pc = new Pc.Builder()
+  .setHDD(200)
+  .build();
+
+console.log(pc);
